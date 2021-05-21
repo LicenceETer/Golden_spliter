9137e8bc9cd8429f69887192277e51aab8cfec70
LicenceETer <hq1995@live.com>
2021-05-21 10:37:01 +0800

    20210521

--- main.py
+++ main.py
@@ -10,31 +10,6 @@ import tushare as ts
 from sqlalchemy import create_engine
 import pymysql
 
-ts.set_token('99684bcfdd007d62f1993d6fc226dda2bf4439104d469ac464360a13')  #设置个人token
-pro = ts.pro_api()
-
-def get_hs300s():
-    "获取中证500股票清单"
-    df = ts.get_hs300s()
-    engine = create_engine('mysql+pymysql://huangqian:huangqian@localhost/Stock?charset=utf8MB4')
-    print(df)
-    df.to_sql('hs300',engine,if_exists='append')
-
-def get_zz500s():
-    "获取中证500股票清单"
-    df = ts.get_zz500s()
-    engine = create_engine('mysql+pymysql://huangqian:huangqian@localhost/Stock?charset=utf8MB4')
-    print(df)
-    df.to_sql('zz500',engine,if_exists='append')
-
-def get_stocks():
-    "获取所有股票清单"
-    #df = ts.get_stock_basics()
-    df = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
-    engine = create_engine('mysql+pymysql://huangqian:huangqian@localhost/Stock?charset=utf8MB4')
-    print(df)
-    df.to_sql('stock_basics',engine,if_exists='append')
-
 def get_daily_data(code):
     "获取对应股票的历史数据"
     #df = ts.get_hist_data(code,start='2020-01-01',end='2021-03-11')  #获取指定股票数据，不复权
--- src/AKGetCodeList.py
+++ src/AKGetCodeList.py
@@ -5,13 +5,27 @@
 
 __author__ = "LicenceETer Huang"
 
+
 import os
 import akshare as ak
 from sqlalchemy import create_engine
 import pymysql
 
-def Getbasic():
+def AKetbasic():
     stock_zh_a_spot_df = ak.stock_zh_a_spot()
     engine = create_engine('mysql+pymysql://huangqian:huangqian@localhost/Stock?charset=utf8MB4')
     stock_zh_a_spot_df.to_sql('AKbasic',engine,if_exists='append')
+
+def AKGetdata(code,start_date,end_date):
+    df = ak.stock_zh_a_daily(code, start_date, end_date, adjust="qfq")
+    engine = create_engine('mysql+pymysql://huangqian:huangqian@localhost/Stock')
+    df['code'] = code
+    df.to_sql('daily_data',engine,if_exists='append')  #使用mysql保存方式
+
+def main():
+    AKGetdata('sz000063','20200303','20210519')
+
+if __name__ == '__main__':
+    main()
+
 
